b46642b0a32afc23af5fa5f4132f41d5f41be2bb
lprimak <lenny@flowlogix.com>
2021-05-30 23:57:32 -0500

    publish web site

---
+++ PublishWebsite.groovy
@@ -0,0 +1,25 @@
+pipeline {
+    agent any
+    environment {
+        ftpcreds = credentials '8695b924-52bd-4fc2-9752-42041489b734'
+    }
+    options {
+        quietPeriod 120
+    }
+    parameters {
+        choice(choices: ['push', 'init', 'catchup'], name: 'publishType', description: 'git ftp command to execute')
+        string(name: 'syncRoot', trim: true, description: 'Synchronization Root Directory', defaultValue: 'docs')
+    }
+
+    stages {
+        stage('Publish Web Site') {
+            steps {
+                sh """ \
+                git ftp $publishType -u \$ftpcreds_USR -p \$ftpcreds_PSW \
+                --syncroot $syncRoot ftpes://web173.dnchosting.com/test_website/$syncRoot \
+                """
+            }
+        }
+    }
+}
+
