bab35b1a510661cf2fe95045520ee0768b15ffe3
lprimak <lenny@flowlogix.com>
2021-09-23 23:28:42 -0500

    xxx

--- vars/stopPayara.groovy
+++ vars/stopPayara.groovy
@@ -2,6 +2,6 @@
 
 def call(def payara_config) {
     if (payara_config.asadmin && payara_config.domain_name) {
-        sh " payara_config.asadmin stop-domain  payara_config.domain_name"
+        sh "$payara_config.asadmin stop-domain $payara_config.domain_name"
     }
 }
