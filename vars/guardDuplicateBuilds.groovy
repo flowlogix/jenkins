421e7e925df6ed62489b049a197f8a56bd139bed
lprimak <lenny@flowlogix.com>
2021-09-23 23:04:11 -0500

    xxx

--- vars/guardDuplicateBuilds.groovy
+++ vars/guardDuplicateBuilds.groovy
@@ -6,19 +6,19 @@ def guardDupBuildsParamsDefault =
     description : 'Please ignore failure - Another build started']
 
 def call(def parameters, Closure cl) {
-    parameters << parameters.withDefault guardDupBuildsParamsDefault.&get
+    parameters = parameters.withDefault guardDupBuildsParamsDefault.&get
     echo "parameters: $parameters"
     if (!parameters.resourceName) {
         parameters.resourceName = "${env.GIT_COMMIT}_$env.JOB_NAME"
     }
-    if (guardDupBuildsParams.neverRan || guardDupBuildsParams.lockSuccess) {
-        guardDupBuildsParams.neverRan = false
-        lock(resource: guardDupBuildsParams.resourceName, skipIfLocked: true) {
+    if (parameters.neverRan || parameters.lockSuccess) {
+        parameters.neverRan = false
+        lock(resource: parameters.resourceName, skipIfLocked: true) {
             cl()
-            guardDupBuildsParams.lockSuccess = true
+            parameters.lockSuccess = true
         }
     }
-    if (!guardDupBuildsParams.lockSuccess) {
+    if (!parameters.lockSuccess) {
         githubNotify description: parameters.description,
         context: parameters.context, status: 'SUCCESS'
         currentBuild.result = 'not_built'
