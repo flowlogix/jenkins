4bcda6e6587861ce85b1619b939a15158ba03b95
lprimak <lenny@flowlogix.com>
2022-09-11 00:31:19 -0400

    removed 'not found' delombok errors and added quality gates to warnings-ng

--- util-lib/vars/checkLogs.groovy
+++ util-lib/vars/checkLogs.groovy
@@ -2,5 +2,6 @@
 
 def call() {
     recordIssues enabledForFailure: true, aggregatingResults: true, tool: java(),
-        filters: [ excludeFile('.*/generated-sources/.*') ]
+        filters: [ excludeFile('.*/generated-sources/.*'), excludeMessage('cannot find symbol') ],
+        qualityGates: [[ threshold: 1, type: 'TOTAL', unstable: true ]]
 }
