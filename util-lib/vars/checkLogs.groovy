c09e34dca2384d49722186f64703da0eae0eb0c1
lprimak <lenny@flowlogix.com>
2022-09-10 23:54:18 -0400

    removed generated source from warnings-ng

--- util-lib/vars/checkLogs.groovy
+++ util-lib/vars/checkLogs.groovy
@@ -1,5 +1,6 @@
 // call the warnings-ng plugin
 
 def call() {
-    recordIssues enabledForFailure: true, aggregatingResults: true, tool: java()
+    recordIssues enabledForFailure: true, aggregatingResults: true, tool: java(),
+        filters: [ excludeFile('.*/generated-sources/.*') ]
 }
