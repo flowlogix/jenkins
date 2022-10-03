08068f2e0b6f156c3947d9b5b1b005290fb0b425
lprimak <lenny@flowlogix.com>
2022-10-03 13:57:10 -0500

    no longer extracting payara from app

--- payara-lib/vars/startPayara.groovy
+++ payara-lib/vars/startPayara.groovy
@@ -6,17 +6,18 @@ import groovy.transform.Field
 @Field
 final def workspace_base = "$WORKSPACE/target/dependency"
 @Field
-final def payara_base = "$workspace_base/payara5"
-@Field
 final def tmpdir = "$workspace_base/tmpdir"
 @Field
 final def portbase = 4900 + (env.EXECUTOR_NUMBER as int) * 100
 
 @Field
 final def payara_default_config =
-    [domain_name : 'domain1', asadmin : "$payara_base/bin/asadmin", domaindir_args: '']
+    [domain_name : 'domain1', payara_version : 'current', asadmin : "$HOME/apps/payara/current/bin/asadmin"]
 
 def call(def payara_config) {
+    if (!payara_config.asadmin && payara_config.payara_version) {
+        payara_default_config.asadmin = "$HOME/apps/payara/$payara_config.payara_version/bin/asadmin"
+    }
     payara_config << payara_default_config + payara_config
     if (!payara_config.domain_name) {
         payara_config.asadmin = null
@@ -27,16 +28,7 @@ def call(def payara_config) {
         return 0
     }
 
-    int result = sh(script: "mvn -B -C dependency:unpack", returnStatus: true)
-    if (result != 0) {
-        echo 'Not able to extract Payara'
-        payara_config.asadmin = null
-    }
-
-    if (!fileExists(payara_config.asadmin)) {
-        payara_config.asadmin = "$HOME/apps/payara/current/bin/asadmin"
-        payara_config.domaindir_args = "--domaindir $workspace_base/payara_domaindir"
-    }
+    payara_config.domaindir_args = "--domaindir $workspace_base/payara_domaindir"
     payara_config.ssl_port = sh(
         script: """$payara_config.asadmin create-domain $payara_config.domaindir_args \
                 --nopassword --portbase $portbase $payara_config.domain_name \
