655516719b54b47b3eb78355ca0f94a8b148607c
lprimak <lenny@flowlogix.com>
2022-10-24 20:43:17 -0500

    added actual resume publish script

---
+++ PublishResume.groovy
@@ -0,0 +1,37 @@
+final def website_host = 'web154.dnchosting.com'
+
+pipeline {
+    agent any
+    environment {
+        ftpcreds = credentials '8695b924-52bd-4fc2-9752-42041489b734'
+    }
+    options {
+        quietPeriod 0
+        disableConcurrentBuilds()
+    }
+    
+    stages {
+        stage('Maven - Asciidoc - convert to html/pdf') {
+            steps {
+                withMaven {
+                    sh """ \
+                    set +x; . "$HOME/.sdkman/bin/sdkman-init.sh"
+                    export JBAKE_OPTS='--add-opens java.base/sun.nio.ch=ALL-UNNAMED \
+                        --add-opens java.base/java.io=ALL-UNNAMED'
+                    set -x
+                    mvn -B -C generate-resources
+                    """
+                }
+            }
+        }
+        stage('Publish - Web Host') {
+            steps {
+                sh """ \
+                lftp -u \$ftpcreds_USR,\$ftpcreds_PSW -e \
+                'mirror -R -P7 -x .git --overwrite --delete --delete-excluded \
+                output hope_website/resume; exit top' $website_host
+                """
+            }
+        }
+    }
+}
