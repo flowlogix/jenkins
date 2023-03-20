44f9da6be1c096e523b59bb96f2672f6d512eb09
lprimak <lenny@flowlogix.com>
2023-03-19 20:07:08 -0500

    using payara 5 for Shiro

--- UnitTests.groovy
+++ UnitTests.groovy
@@ -20,6 +20,7 @@ pipeline {
                     currentBuild.description = "Working on git commit ${env.GIT_COMMIT[0..7]} Node $env.NODE_NAME"
                     if (env.GIT_URL.contains('shiro')) {
                         payara_config.jacoco_expr_args = '-pl :jakarta-ee-support'
+                        payara_config.payara_version = 'payara-5'
                         payara_config.force_start = true
                         payara_config.jacoco_tcp_server = false
                     }
