f8fc651660a8fa369af4906333f4f352f2d67769
lprimak <lenny@flowlogix.com>
2023-04-08 19:09:43 -0500

    deploy docs during integration tests

--- IntegrationTests.groovy
+++ IntegrationTests.groovy
@@ -4,9 +4,13 @@ final def profiles           = 'payara-server-remote,coverage,all-tests'
 final def profiles_no_stress = 'payara-server-remote,coverage,ui-test'
 def payara_config = [ domain_name : 'test-domain', jacoco_profile : profiles ]
 def mvnCommandLine
+final def website_host = 'web154.dnchosting.com'
 
 pipeline {
     agent any
+    environment {
+        ftpcreds = credentials '8695b924-52bd-4fc2-9752-42041489b734'
+    }
 
     options {
         disableConcurrentBuilds()
@@ -72,8 +76,9 @@ pipeline {
                 export MAVEN_OPTS="\$MAVEN_OPTS --add-opens java.base/sun.nio.ch=ALL-UNNAMED \
                     --add-opens java.base/java.io=ALL-UNNAMED"
                 mvn -B -C -ntp process-resources -f ${env.WORKSPACE}/docs/
+                lftp -u \$ftpcreds_USR,\$ftpcreds_PSW -e 'mirror -R -P7 --overwrite --delete \
+                ${env.WORKSPACE}/docs/target/output flowlogix_docs; exit top' $website_host
                 """
-                // TODO: deploy docs to the web site
             }
         }
     }
