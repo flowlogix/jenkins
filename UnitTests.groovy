944b8d1aa42b000eaff3a1caa95c2280ca744dfe
lprimak <lenny@flowlogix.com>
2023-12-21 12:48:54 -0600

    enh(unit-tests): factored out checkout step

--- UnitTests.groovy
+++ UnitTests.groovy
@@ -3,9 +3,8 @@
 
 def profiles = 'payara-server-remote,ui-test,coverage,ci'
 def payara_config = [ domain_name : 'test-domain' ]
+def mvn_cmd = 'mvn'
 def extra_build_options = ''
-
-payara_config << [ jacoco_profile : profiles ]
 
 pipeline {
     agent any
@@ -15,9 +14,8 @@ pipeline {
     }
 
     stages {
-        stage('Maven Info') {
+        stage('Checkout and Setup') {
             steps {
-                sh "mvn -V -B -ntp -C -N -P$profiles help:all-profiles"
                 script {
                     currentBuild.description = "Commit ${env.GIT_COMMIT[0..7]} Node $env.NODE_NAME"
                     if (env.GIT_URL.contains('shiro')) {
@@ -25,7 +23,13 @@ pipeline {
                         // suppress OpenLiberty plugin warnings
                         extra_build_options = '-Dorg.slf4j.simpleLogger.log.io.openliberty.tools.maven=error'
                     }
+                    payara_config << [ jacoco_profile : profiles ]
                 }
+            }
+        }
+        stage('Maven Info') {
+            steps {
+                sh "$mvn_cmd -V -B -ntp -C -N -P$profiles help:all-profiles"
             }
         }
         stage('Start Payara') {
@@ -39,7 +43,7 @@ pipeline {
                     sh """
                     export MAVEN_OPTS="\$MAVEN_OPTS $JAVA_TOOL_OPTIONS"
                     unset JAVA_TOOL_OPTIONS
-                    mvn -B -ntp -C verify -fae -P$profiles \$(eval echo \$MAVEN_ADD_OPTIONS) \
+                    $mvn_cmd -B -ntp -C verify -fae -P$profiles \$(eval echo \$MAVEN_ADD_OPTIONS) \
                     -Dwebdriver.chrome.binary="\$(eval echo \$CHROME_BINARY)" \
                     -Dmaven.test.failure.ignore=true -DtrimStackTrace=false \
                     -Dmaven.install.skip=true -DadminPort=$payara_config.admin_port \
