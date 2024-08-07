a5f263785539957e8009b3fde93a493842ec8e69
lprimak <lenny@flowlogix.com>
2024-08-07 11:57:26 -0500

    deps(loggins): added new slf4j logging

--- util-lib/vars/checkLogs.groovy
+++ util-lib/vars/checkLogs.groovy
@@ -41,6 +41,7 @@ def call(String log_pattern, boolean checkConsole = true, qualityThreshold = 1) 
             /(javax|jakarta).faces.FacesException: #\{exceptionBean.throwExceptionFromMethod\(\)}:[\s\S]*/),
             excludeMessage(/java.io.IOException: Connection is closed/),
             excludeMessage(/JSF1064: Unable to find or serve resource.*/),
+            excludeMessage(/SLF4J\(I\): Connected with provider of type.*/),
             excludeMessage(/The web application.*created a ThreadLocal.*value.*org.testng.internal.TestResult.*TestR.*/),
             excludeMessage(/Setting .* is unknown and will be ignored/),
             excludeMessage(/Unprocessed event : UnprocessedChangeEvent.*/) ]
