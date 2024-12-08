5759441905858ba9489120b6e6cc0d488519e2e1
lprimak <lenny@flowlogix.com>
2024-12-08 03:04:59 -0600

    chore: added maven settings credentials

--- util-lib/vars/mavenSettingsCredentials.groovy
+++ util-lib/vars/mavenSettingsCredentials.groovy
@@ -5,10 +5,15 @@ import groovy.transform.Field
 final String flowlogixCredentialsId = '8faf3947-efe5-4733-86d9-b3e08faa91d1'
 @Field
 final String apacheCredentialsId = 'ddb50232-4f82-4f93-90cc-3e1a6b2a28da'
+@Field
+final String mavenSettingsCredentialsId = 'a2decb6f-0847-4fa4-8cd1-57acee932efc'
 
 void call(boolean isApache, Closure cl) {
-    withCredentials([string(credentialsId: isApache ? apacheCredentialsId : flowlogixCredentialsId,
-            variable: 'MAVEN_GPG_PASSPHRASE')]) {
+    withCredentials(
+            [string(credentialsId: isApache ? apacheCredentialsId : flowlogixCredentialsId,
+                    variable: 'MAVEN_GPG_PASSPHRASE')],
+            [string(credentialsId: mavenSettingsCredentialsId,
+                    variable: 'MAVEN_SETTINGS_MASTER_PASSWORD')]) {
         cl.call()
     }
 }
