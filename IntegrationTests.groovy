f381344088bc3f4b56d47d6f3807a644afb23a8c
lprimak <lenny@flowlogix.com>
2024-12-09 06:38:54 -0600

    bugfix: GIT_BRANCH env var for integration tests

--- IntegrationTests.groovy
+++ IntegrationTests.groovy
@@ -24,7 +24,7 @@ pipeline {
                 sh "mvn -V -B -ntp -C -N -P$profiles help:all-profiles"
                 script {
                     currentBuild.description = "Commit ${env.GIT_COMMIT[0..7]} Node $env.NODE_NAME"
-                    if (env.GIT_URL.contains('flowlogix/flowlogix') && env.CHANGE_TARGET == '5.x') {
+                    if (env.GIT_URL.contains('flowlogix/flowlogix') && env.GIT_BRANCH == '5.x') {
                         qualityThreshold = 2
                     }
                 }
